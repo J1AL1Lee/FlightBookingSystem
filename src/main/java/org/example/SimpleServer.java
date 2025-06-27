03347fec1f6ab0342283bcca3cc7c3f19c9e7963
LJL <yaoqih267@gmail.com>
2025-06-27 17:25:50 +0800

    Initial commit

---
+++ src/main/java/org/example/SimpleServer.java
@@ -0,0 +1,7 @@
+// 先创建一个超简单的HTTP服务器
+public class SimpleServer {
+    public static void main(String[] args) {
+        System.out.println("Hello World! 服务器启动中...");
+        // 最简单的HTTP响应
+    }
+}
