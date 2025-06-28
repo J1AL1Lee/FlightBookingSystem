97dd6fe49ebed9b222a39d8a41d966d4febc5741
LJL <yaoqih267@gmail.com>
2025-06-28 10:06:15 +0800

    Initial commit

--- src/main/java/dao/DatabaseConnection.java
+++ src/main/java/dao/DatabaseConnection.java
@@ -11,7 +11,7 @@ public class DatabaseConnection {
             "allowPublicKeyRetrieval=true&useSSL=false";
 
     private static final String USERNAME = "root";  // 改成你的用户名
-    private static final String PASSWORD = "43436Ljl"; // 改成你的密码
+    private static final String PASSWORD = ""; // 改成你的密码
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USERNAME, PASSWORD);
